fb7bc773d66e92f20a3f37d88880c6b2a049b0dd
Aziz Yakubjanov <yakubjanov0@hotmail.com>
2017-04-03 21:54:17 +0000

    init

---
+++ public/main.js
@@ -0,0 +1,24 @@
+/*global firebase*/
+var provider = new firebase.auth.GoogleAuthProvider();
+let userData = null;
+firebase.auth().onAuthStateChanged(function(user){
+  userData = user;
+  if (!user) {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  }
+});
